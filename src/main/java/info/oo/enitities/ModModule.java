32c8bd4cc9d4a917fe5138376d9939df434aa632
NicolyKuhn13 <nicolycolorsk@gmail.com>
2021-05-21 18:17:04 -0300

    Criação das classes "User" e "ModModule"

---
+++ src/main/java/info/oo/enitities/ModModule.java
@@ -0,0 +1,13 @@
+package info.oo.enitities;
+
+public class ModModule {
+    
+    private String name;
+    private String mineVersion;
+    private User user;
+
+
+    
+
+
+}
---
+++ src/main/java/info/oo/enitities/User.java
@@ -0,0 +1,11 @@
+package info.oo.enitities;
+
+public class User {
+
+    private String name;
+    private String login;
+    private String password;
+    
+    
+
+}
