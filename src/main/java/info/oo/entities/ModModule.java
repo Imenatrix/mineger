b921e89c3abb82014a5f82edf9c2dc808244b5ca
NicolyKuhn13 <nicolycolorsk@gmail.com>
2021-06-01 00:21:43 -0300

    Criação do método construtor na classe "ModModule"

--- src/main/java/info/oo/entities/ModModule.java
+++ src/main/java/info/oo/entities/ModModule.java
@@ -8,6 +8,15 @@ public class ModModule {
     private String mineVersion;
     private User user;
     private ArrayList<Mod> mods = new ArrayList<Mod>();
+    private ModLoader modLoader;
+
+    public ModModule (String name, String mineVersion, User user, Mod mods, ModLoader modLoader){
+        this.name = name;
+        this.mineVersion = mineVersion;
+        this.user = user;
+        this.mods.add(mods);
+        this.modLoader = modLoader;
+    }
 
 
 
