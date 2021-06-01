dcb167def6d51b7674ea6cac009bb4503aa9a0be
NicolyKuhn13 <nicolycolorsk@gmail.com>
2021-06-01 00:27:51 -0300

    Inserção dos atributos da classe "Mod"

--- src/main/java/info/oo/entities/Mod.java
+++ src/main/java/info/oo/entities/Mod.java
@@ -2,7 +2,13 @@ package info.oo.entities;
 
 public class Mod {
 
+    private String name;
+    private java.net.URL link;
+    private String version;
+    private String mineVersion;
+    private String description;
+    private ModModule modModule;
+    private ModOrigin modOrigin;
 
-    
 
 }
