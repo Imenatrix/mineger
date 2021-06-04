6b67f2af2271817b5ff00d0ec08c072d1bf37525
Ruan Gomes <ruangomesdorosario@gmail.com>
2021-06-04 15:41:42 -0300

    Remove minecraftVersion de Mod

--- src/main/java/info/oo/entities/Mod.java
+++ src/main/java/info/oo/entities/Mod.java
@@ -7,16 +7,14 @@ public class Mod {
     private int id;
     private String name;
     private URL url;
-    private String minecraftVersion;
     private String summary;
     private ModModule modModule;
     private ModOrigin modOrigin;
 
-    public Mod(int id, String name, URL url, String minecraftVersion, String summary, ModModule modModule, ModOrigin modOrigin) {
+    public Mod(int id, String name, URL url, String summary, ModModule modModule, ModOrigin modOrigin) {
         this.id = id;
         this.name = name;
         this.url = url;
-        this.minecraftVersion = minecraftVersion;
         this.summary = summary;
         this.modModule = modModule;
         this.modOrigin = modOrigin;
