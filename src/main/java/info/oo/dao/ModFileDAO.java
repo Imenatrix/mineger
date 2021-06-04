bc787e22ea21db62edce751dad11b29365998985
Ruan Gomes <ruangomesdorosario@gmail.com>
2021-06-04 20:46:13 -0300

    Cria uma implementação basica de ModFileDAO

---
+++ src/main/java/info/oo/dao/ModFileDAO.java
@@ -0,0 +1,47 @@
+package info.oo.dao;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import info.oo.dao.interfaces.IModFileDAO;
+import info.oo.database.ConnectionFactory;
+import info.oo.entities.ModFile;
+
+public class ModFileDAO implements IModFileDAO {
+    
+    public ArrayList<ModFile> getAll() {
+        String query = "select * from mod_file;";
+        ArrayList<ModFile> modFiles = new ArrayList<ModFile>();
+
+        try (
+            Connection conn = ConnectionFactory.getConnection();
+            PreparedStatement stmt = conn.prepareStatement(query);
+            ResultSet result = stmt.executeQuery();
+        ) {
+            while(result.next()) {
+                ModFile modFile = new ModFile(
+                    result.getInt("id"),
+                    result.getString("file_name"),
+                    new URL(result.getString("url")),
+                    result.getString("minecraft_version"),
+                    null
+                );
+                modFiles.add(modFile);
+            }
+        }
+        catch (MalformedURLException e) {
+            System.out.println(e);
+        }
+        catch (SQLException e) {
+            System.out.println(e);
+        }
+
+        return modFiles;
+    }
+
+}
---
+++ src/main/java/info/oo/dao/interfaces/IModFileDAO.java
@@ -0,0 +1,10 @@
+package info.oo.dao.interfaces;
+
+import java.util.ArrayList;
+import info.oo.entities.ModFile;
+
+public interface IModFileDAO {
+    
+    public ArrayList<ModFile> getAll();
+
+}
