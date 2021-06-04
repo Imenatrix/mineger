b37be796a39b9fc2379c613475c1b9d6707b52d8
Ruan Gomes <ruangomesdorosario@gmail.com>
2021-06-04 15:27:57 -0300

    Implementa construtor para Mod

--- src/main/java/info/oo/entities/Mod.java
+++ src/main/java/info/oo/entities/Mod.java
@@ -9,5 +9,14 @@ public class Mod {
     private ModModule modModule;
     private ModOrigin modOrigin;
 
+    public Mod(String name, java.net.URL url, String mineVersion, String summary, ModModule modModule, ModOrigin modOrigin) {
+        this.name = name;
+        this.url = url;
+        this.mineVersion = mineVersion;
+        this.summary = summary;
+        this.modModule = modModule;
+        this.modOrigin = modOrigin;
+    }
+
 
 }
