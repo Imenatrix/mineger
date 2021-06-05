976965e805112d559129c28305c579e9038002ce
Ruan Gomes <ruangomesdorosario@gmail.com>
2021-06-05 16:48:42 -0300

    Carrega modLoader e modOrigin em ModDAO
    Agora, quando ModDAO.getAll é chamada, os itens do retorno devem conter
    seus respectivos ModLoaders e ModOrigins.

--- src/main/java/info/oo/dao/ModDAO.java
+++ src/main/java/info/oo/dao/ModDAO.java
@@ -7,12 +7,23 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
+
 import info.oo.dao.interfaces.IModDAO;
+import info.oo.dao.interfaces.IModLoaderDAO;
+import info.oo.dao.interfaces.IModOriginDAO;
 import info.oo.database.ConnectionFactory;
 import info.oo.entities.Mod;
 
 public class ModDAO implements IModDAO {
-    
+
+    private IModLoaderDAO modLoaderDAO;
+    private IModOriginDAO modOriginDAO;
+
+    public ModDAO(IModLoaderDAO modLoaderDAO, IModOriginDAO modOriginDAO) {
+        this.modLoaderDAO = modLoaderDAO;
+        this.modOriginDAO = modOriginDAO;
+    }
+
     public ArrayList<Mod> getAll() {
         String query = "select * from `mod`;";
         ArrayList<Mod> mods = new ArrayList<Mod>();
@@ -28,8 +39,8 @@ public class ModDAO implements IModDAO {
                     result.getString("name"),
                     new URL(result.getString("url")),
                     result.getString("summary"),
-                    null,
-                    null
+                    modLoaderDAO.getById(result.getInt("mod_loader_id")),
+                    modOriginDAO.getById(result.getInt("mod_origin_id"))
                 );
                 mods.add(mod);
             }
