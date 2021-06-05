11e8ff62c84d9c97c444575426ed7c4f4a5615e6
Ruan Gomes <ruangomesdorosario@gmail.com>
2021-06-05 16:45:14 -0300

    Carrega user e modOrigin em ModModuleDAO
    Agora, quando ModModuleDAO.getAll for chamada, os itens do retorno devem
    conter seus respectivos Users e ModOrigins.

--- src/main/java/info/oo/dao/ModModuleDAO.java
+++ src/main/java/info/oo/dao/ModModuleDAO.java
@@ -6,11 +6,21 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
+import info.oo.dao.interfaces.IModLoaderDAO;
 import info.oo.dao.interfaces.IModModuleDAO;
+import info.oo.dao.interfaces.IUserDAO;
 import info.oo.database.ConnectionFactory;
 import info.oo.entities.ModModule;
 
 public class ModModuleDAO implements IModModuleDAO {
+
+    private IUserDAO userDAO;
+    private IModLoaderDAO modLoaderDAO;
+
+    public ModModuleDAO(IUserDAO userDAO, IModLoaderDAO modLoaderDAO) {
+        this.userDAO = userDAO;
+        this.modLoaderDAO = modLoaderDAO;
+    }
     
     public ArrayList<ModModule> getAll() {
 
@@ -27,9 +37,9 @@ public class ModModuleDAO implements IModModuleDAO {
                     result.getInt("id"),
                     result.getString("name"),
                     result.getString("minecraft_version"),
+                    userDAO.getById(result.getInt("user_id")),
                     null,
-                    null,
-                    null
+                    modLoaderDAO.getById(result.getInt("mod_loader_id"))
                 );
                 modModules.add(modModule);
             }
