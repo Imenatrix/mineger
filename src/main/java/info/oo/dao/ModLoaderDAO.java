2fa5321a0827611ebd7ec197d92459ffeed21ad3
Ruan Gomes <ruangomesdorosario@gmail.com>
2021-06-14 15:53:58 -0300

    Refatora ModLoaderDAO usando Clarice

--- src/main/java/info/oo/dao/ModLoaderDAO.java
+++ src/main/java/info/oo/dao/ModLoaderDAO.java
@@ -2,16 +2,14 @@ package info.oo.dao;
 
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Optional;
 
 import info.oo.dao.interfaces.IModLoaderDAO;
-import info.oo.database.ConnectionFactory;
 import info.oo.entities.ModLoader;
+import info.oo.utils.Clarice;
 
 public class ModLoaderDAO implements IModLoaderDAO {
 
@@ -29,62 +27,50 @@ public class ModLoaderDAO implements IModLoaderDAO {
             return optionalModLoader.get();
         }
 
-        ModLoader modLoader = null;
         String query = "select * from mod_loader where id = ?";
-
-        try (
-            Connection conn = ConnectionFactory.getConnection();
-            PreparedStatement stmt = conn.prepareStatement(query);
-        ) {
-            stmt.setInt(1, id);
-
-            try (ResultSet result = stmt.executeQuery()) {
+        return Clarice.executeQueryOr(
+            query,
+            stmt -> stmt.setInt(1, id),
+            result -> {
                 result.next();
-                modLoader = new ModLoader(
-                    result.getInt("id"),
-                    result.getString("name"),
-                    new URL(result.getString("url"))
-                );
-            }
-        }
-        catch (SQLException e) {
-            System.out.println(e);
-        }
-        catch (MalformedURLException e) {
-            System.out.println(e);
-        } 
-
-        cache.add(modLoader);
-        return modLoader;
-
+                ModLoader modLoader = resultToModLoader(result);
+                cache.add(modLoader);
+                return modLoader;
+            },
+            null
+        );
     }
     
     public ArrayList<ModLoader> getAll() {
         String query = "select * from mod_loader;";
-        ArrayList<ModLoader> modLoaders = new ArrayList<ModLoader>();
+        return Clarice.executeQueryOr(
+            query,
+            stmt -> {},
+            result -> resultToModLoaderArrayList(result),
+            new ArrayList<ModLoader>()
+        );
+    }
 
-        try (
-            Connection conn = ConnectionFactory.getConnection();
-            PreparedStatement stmt = conn.prepareStatement(query);
-            ResultSet result = stmt.executeQuery();
-        ) {
-            while(result.next()) {
-                ModLoader modLoader = new ModLoader(
-                    result.getInt("id"),
-                    result.getString("name"),
-                    new URL(result.getString("url"))
-                );
-                modLoaders.add(modLoader);
-            }
+    private ArrayList<ModLoader> resultToModLoaderArrayList(ResultSet result) throws SQLException {
+        ArrayList<ModLoader> modLoaders = new ArrayList<ModLoader>();
+        while(result.next()) {
+            modLoaders.add(resultToModLoader(result));
+        }
+        return modLoaders;
+    }
+
+    private ModLoader resultToModLoader(ResultSet result) throws SQLException{
+        try {
+            return new ModLoader(
+                result.getInt("id"),
+                result.getString("name"),
+                new URL(result.getString("url"))
+            );
         }
         catch (MalformedURLException e) {
             System.out.println(e);
         }
-        catch (SQLException e) {
-            System.out.println(e);
-        }
-
-        return modLoaders;
+        return null;
     }
 
 }
