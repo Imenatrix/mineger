938c0e9f19678939f0398eb1bd0e22805b5c2f49
NicolyKuhn13 <nicolycolorsk@gmail.com>
2021-06-14 19:09:01 -0300

    Implementação da classe Login

---
+++ src/main/java/info/oo/gui/pages/Login.java
@@ -0,0 +1,23 @@
+package info.oo.gui.pages;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class Login {
+
+    @FXML
+    private TextField tfLogin;
+
+    @FXML
+    private PasswordField pfPassword;
+
+    @FXML 
+    private Button btnEntrar;
+
+    public void login(){
+
+    } 
+    
+}
