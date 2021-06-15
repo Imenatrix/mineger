fb534fce2aabcb1057b7a14b988c0d7cfe53b023
Ruan Gomes <ruangomesdorosario@gmail.com>
2021-06-15 19:51:23 -0300

    Implementa paginação com busca em ModFileDAO

--- src/main/java/info/oo/dao/interfaces/IModFileDAO.java
+++ src/main/java/info/oo/dao/interfaces/IModFileDAO.java
@@ -10,5 +10,7 @@ public interface IModFileDAO {
     public ArrayList<ModFile> getAllByModModuleId(int id);
     public int getTotalPagesByModLoaderIdAndMinecraftVersion(int limit, int modLoaderId, String minecraftVersion);
     public ArrayList<ModFile> getPaginatedByModLoaderIdAndMinecraftVersion(int limit, int page, int modLoaderId, String minecraftVersion);
+    public ArrayList<ModFile> getPaginatedByModLoaderIdAndMinecraftVersionAndSearch(int limit, int page, int modLoaderId, String minecraftVersion, String search);
+    public int getTotalPagesByModLoaderIdAndMinecraftVersionAndSearch(int limit, int page, int modLoaderId, String minecraftVersion, String search);
 
 }
