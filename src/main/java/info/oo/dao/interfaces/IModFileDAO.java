d69b04c4b82fb22eaa2b80f8dc6d84dc199fcaf0
Ruan Gomes <ruangomesdorosario@gmail.com>
2021-06-15 23:20:17 -0300

    Muda int para Integer em IModFileDAO

--- src/main/java/info/oo/dao/interfaces/IModFileDAO.java
+++ src/main/java/info/oo/dao/interfaces/IModFileDAO.java
@@ -8,9 +8,6 @@ public interface IModFileDAO {
     public ModFile getById(int id);
     public ArrayList<ModFile> getAll();
     public ArrayList<ModFile> getAllByModModuleId(int id);
-    public int getTotalPages(int limit, int modLoaderId, String minecraftVersion);
-    public int getTotalPages(int limit, int modLoaderId, String minecraftVersion, String search);
-    public ArrayList<ModFile> getPaginated(int limit, int page, int modLoaderId, String minecraftVersion);
-    public ArrayList<ModFile> getPaginated(int limit, int page, int modLoaderId, String minecraftVersion, String search);
-
+    public int getTotalPages(int limit, Integer modLoaderId, Integer modOriginId, String minecraftVersion, String search);
+    public ArrayList<ModFile> getPaginated(int limit, int page, Integer modLoaderId, Integer modOriginId, String minecraftVersion, String search);
 }
