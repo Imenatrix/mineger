21074e2bb82776b1f04259390b5fcfd50d63c9c5
Ruan Gomes <ruangomesdorosario@gmail.com>
2021-06-23 18:48:32 -0300

    Adiciona UserModModulesUnitOfWork
    Essa UnitOfWork representa as mudanças feitas na lista modModules de um
    User.

---
+++ src/main/java/info/oo/repositories/unitsOfWork/UserModModulesUnitOfWork.java
@@ -0,0 +1,45 @@
+package info.oo.repositories.unitsOfWork;
+
+import java.util.ArrayList;
+
+import info.oo.dao.interfaces.IModModuleDAO;
+import info.oo.entities.ModModule;
+import info.oo.entities.User;
+
+public class UserModModulesUnitOfWork {
+
+    private User user;
+    private ArrayList<ModModule> insertedModModules;
+    private ArrayList<ModModule> deletedModModules;
+    private IModModuleDAO modModuleDAO;
+    
+    public UserModModulesUnitOfWork(User user, ArrayList<ModModule> modModules, ArrayList<ModModule> oldModModules, IModModuleDAO modModuleDAO) {
+
+        this.user = user;
+        this.modModuleDAO = modModuleDAO;
+
+        insertedModModules = new ArrayList<ModModule>(modModules);
+        insertedModModules.removeIf(item -> 
+            oldModModules.stream()
+                .map(item2 -> item2.getId())
+                .anyMatch(item2 -> item2 == item.getId())
+        );
+
+        deletedModModules = new ArrayList<ModModule>(oldModModules);
+        deletedModModules.removeIf(item -> 
+            modModules.stream()
+                .map(item2 -> item2.getId())
+                .anyMatch(item2 -> item2 == item.getId())
+        );
+    }
+
+    public void commit() {
+        for (ModModule modModule : insertedModModules) {
+            modModuleDAO.insert(modModule, user);
+        }
+        for (ModModule modModule : deletedModModules) {
+            modModuleDAO.delete(modModule);
+        }
+    }
+
+}
