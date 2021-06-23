30f87418949d670dc12f2c65f95b6a4623d8abbb
Ruan Gomes <ruangomesdorosario@gmail.com>
2021-06-23 13:22:40 -0300

    Altera Login para acessar User por UserRepository

--- src/main/java/info/oo/gui/pages/Login.java
+++ src/main/java/info/oo/gui/pages/Login.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 
 import info.oo.dao.interfaces.IUserDAO;
 import info.oo.entities.User;
+import info.oo.repositories.interfaces.IUserRepository;
 import info.oo.utils.VoidCallback;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
@@ -27,12 +28,14 @@ public class Login extends VBox {
     private TextField txtPassword;
 
     private IUserDAO userDAO;
+    private IUserRepository userRepository;
     private VoidCallback<User> onSuccess;
 
-    public Login(IUserDAO userDAO,  VoidCallback<User> onSuccess) {
+    public Login(IUserDAO userDAO, IUserRepository userRepository,  VoidCallback<User> onSuccess) {
         super();
         loadFXML();
         this.userDAO = userDAO;
+        this.userRepository = userRepository;
         this.onSuccess = onSuccess;
         Platform.runLater(() -> requestFocus());
     }
@@ -53,11 +56,12 @@ public class Login extends VBox {
     void onBtnLoginAction(ActionEvent event) {
         String login = txtLogin.getText();
         String password = txtPassword.getText();
-        User user = userDAO.getByLoginAndPassword(login, password);
-        if (user == null) {
+        User loginUser = userDAO.getByLoginAndPassword(login, password);
+        if (loginUser == null) {
             lblError.setText("Login ou senha incorretos.");
         }
         else {
+            User user = userRepository.getById(loginUser.getId());
             onSuccess.call(user);
         }
     }
