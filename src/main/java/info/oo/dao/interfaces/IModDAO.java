8e080ad72c36abb3347a9b96c3f8014f70012d2f
Ruan Gomes <ruangomesdorosario@gmail.com>
2021-06-24 02:16:01 -0300

    Implementa query por dependente em ModDAO

--- src/main/java/info/oo/dao/interfaces/IModDAO.java
+++ src/main/java/info/oo/dao/interfaces/IModDAO.java
@@ -1,7 +1,11 @@
 package info.oo.dao.interfaces;
 
+import java.util.ArrayList;
+
 import info.oo.entities.Mod;
+import info.oo.entities.ModFile;
 
 public interface IModDAO {
     public Mod getById(int id);
+    public ArrayList<Mod> getAllByModFiles(ArrayList<ModFile> modFiles);
 }
