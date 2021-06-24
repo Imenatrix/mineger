0cdc93779721098a1e75a6eb47798f5aa22b135b
Ruan Gomes <ruangomesdorosario@gmail.com>
2021-06-24 02:16:24 -0300

    Implementa query por dependente em ModFileDAO

--- src/main/java/info/oo/dao/interfaces/IModFileDAO.java
+++ src/main/java/info/oo/dao/interfaces/IModFileDAO.java
@@ -1,12 +1,15 @@
 package info.oo.dao.interfaces;
 
 import java.util.ArrayList;
+
+import info.oo.entities.FileModule;
 import info.oo.entities.ModFile;
 
 public interface IModFileDAO {
     
     public ModFile getById(int id);
     public ArrayList<ModFile> getAllByModModuleId(int id);
+    public ArrayList<ModFile> getByFileModules(ArrayList<FileModule> fileModules);
     public int getTotalPages(int limit, Integer modLoaderId, Integer modOriginId, String minecraftVersion, String search);
     public ArrayList<ModFile> getPage(int limit, int page, Integer modLoaderId, Integer modOriginId, String minecraftVersion, String search);
 }
